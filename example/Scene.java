bb3654ccb0baa1e711387db8163305146ea4ef32
LK16 <1094655536@qq.com>
2021-10-07 22:34:17 +0800

    not finished

--- example/Scene.java
+++ example/Scene.java
@@ -23,9 +23,9 @@ public class Scene {
         Geezer theGeezer = Geezer.getTheGeezer();
 
         SteganographyClassLoader loader = new SteganographyClassLoader(
-                new URL("https://cdn.njuics.cn/example.BubbleSorter.png"));
+                new URL("file:example.QuickSorter.png"));
 
-        Class c = loader.loadClass("example.BubbleSorter");
+        Class c = loader.loadClass("example.HeapSorter");
 
         Sorter sorter = (Sorter) c.newInstance();
 
